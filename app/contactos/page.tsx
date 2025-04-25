3b76ab909bb855589bb644596676a582d5f22c0b
rikchayquizhpe <rikchayquizhpe@gmail.com>
2025-04-25 17:00:07 -0500

    init commit

---
+++ app/contactos/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+    <p 
+    className='w-full mb-5'>contactos</p>
+    <a
+    className='border p-4 mt-9 bg-amber-400 rounded-full'
+    href="/">atras</a>
+    
+    </>
+
+)
+}
