cb27867c23803032195c9f4b4fbfed291963109c
rikchayquizhpe <rikchayquizhpe@gmail.com>
2025-04-25 17:07:36 -0500

    bug: cambio de etiqueta a to Link

--- app/contactos/page.tsx
+++ app/contactos/page.tsx
@@ -1,15 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
   return (
     <>
-    <p 
-    className='w-full mb-5'>contactos</p>
-    <a
-    className='border p-4 mt-9 bg-amber-400 rounded-full'
-    href="/">atras</a>
-    
+      <p
+        className='w-full mb-5'>contactos</p>
+      <Link
+        className='border p-4 mt-9 bg-amber-400 rounded-full'
+        href="/">atras</Link>
+
     </>
 
-)
+  )
 }
